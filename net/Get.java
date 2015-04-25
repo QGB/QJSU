f37473405ad5a6a4136f9863224cb5251599a80d
QGB <qgbcs1@gmail.com>
2015-04-25 17:14:26 +0800

    html

--- net/Get.java
+++ net/Get.java
@@ -137,9 +137,25 @@ public class Get {
 		return doc;
 	}
 
-	public static String html(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public static String html(String path) throws Exception {
+		URL url = new URL(path);
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setConnectTimeout(5 * 1000);
+		InputStream inStream = conn.getInputStream();//通过输入流获取html数据
+		byte[] data = readInputStream(inStream);//得到html的二进制数据
+		String html = new String(data, "gb2312");
+		return html;
+	}
+	public static byte[] readInputStream(InputStream inStream) throws Exception{
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while( (len=inStream.read(buffer)) != -1 ){
+			outStream.write(buffer, 0, len);
+		}
+		inStream.close();
+		return outStream.toByteArray();
 	}
 	
 }
